5ef8d606beb429d522b3fcfbdcb6b843b817db50
Your Name <you@example.com>
2021-07-19 11:58:33 +0300

    testng passed

--- src/main/java/ru/netology/service/CashbackHackService.java
+++ src/main/java/ru/netology/service/CashbackHackService.java
@@ -1,9 +1,9 @@
 package ru.netology.service;
 
 public class CashbackHackService {
-    private final int boundary = 1000;
+    private static final int boundary = 1000;
 
-    public int remain(int amount) {
+    public static int remain(int amount) {
         return boundary - amount % boundary;
     }
 }
---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,44 @@
+
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+    private CashbackHackService cashbackHackService = new CashbackHackService();
+
+    @Test
+    public void checkWhen1() {
+        assertEquals(cashbackHackService.remain(1), 999);
+    }
+
+    @Test
+    public void checkWhen1000() {
+        assertEquals(cashbackHackService.remain(1000), 0);
+    }
+
+    @Test
+    public void checkWhenAboveThousand() {
+        assertEquals(cashbackHackService.remain(1100), 900);
+    }
+
+    @Test
+    public void negativeAmount() {
+        assertEquals(cashbackHackService.remain(-100), 1100);
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
